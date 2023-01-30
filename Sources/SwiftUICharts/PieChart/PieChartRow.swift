b80054d27dfbc94c1263c4ae34842b229abb7c4d
dezhest <dezhest@yandex.ru>
2023-01-30 14:29:17 +0400

    Row fixes

---
+++ Sources/SwiftUICharts/PieChart/PieChartRow.swift
@@ -0,0 +1,84 @@
+//
+//  PieChartRow.swift
+//  ChartView
+//
+//  Created by András Samu on 2019. 06. 12..
+//  Copyright © 2019. András Samu. All rights reserved.
+//
+
+import SwiftUI
+
+public struct PieChartRow: View {
+    var data: [PieChartData]
+    var labeledData: [(String, Double)]?
+    var backgroundColor: Color
+    var accentColor: Color
+    var slices: [PieSlice] {
+        var tempSlices: [PieSlice] = []
+        var lastEndDeg: Double = 0
+        let maxValue = data.map({
+            $0.value
+        }).reduce(0, +)
+        for slice in data {
+            let normalized: Double = Double(slice.value)/Double(maxValue)
+            let startDeg = lastEndDeg
+            let endDeg = lastEndDeg + (normalized * 360)
+            lastEndDeg = endDeg
+            tempSlices.append(PieSlice(startDeg: startDeg, endDeg: endDeg, value: slice.value, label: slice.label, normalizedValue: normalized))
+        }
+        return tempSlices
+    }
+
+    @Binding var showValue: Bool
+    @Binding var currentValue: PieChartData
+
+    @State private var currentTouchedIndex = -1 {
+        didSet {
+            if oldValue != currentTouchedIndex {
+                showValue = currentTouchedIndex != -1
+                currentValue = showValue ? PieChartData(label: slices[currentTouchedIndex].label, value: slices[currentTouchedIndex].value) : PieChartData(value: 0)
+            }
+        }
+    }
+
+    public var body: some View {
+        GeometryReader { geometry in
+            ZStack {
+                if self.slices.count > 0 {
+                    ForEach(0..<self.slices.count, id: \.self) { i in
+                        PieChartCell(rect: geometry.frame(in: .local), startDeg: self.slices[i].startDeg, endDeg: self.slices[i].endDeg, index: i, backgroundColor: self.backgroundColor, accentColor: self.accentColor)
+                            .scaleEffect(self.currentTouchedIndex == i ? 1.1 : 1)
+                            .animation(Animation.spring())
+                    }
+                }
+            }
+            .gesture(DragGesture()
+                        .onChanged({ value in
+                            let rect = geometry.frame(in: .local)
+                            let isTouchInPie = isPointInCircle(point: value.location, circleRect: rect)
+                            if isTouchInPie {
+                                let touchDegree = degree(for: value.location, inCircleRect: rect)
+                                self.currentTouchedIndex = self.slices.firstIndex(where: { $0.startDeg < touchDegree && $0.endDeg > touchDegree }) ?? -1
+                            } else {
+                                self.currentTouchedIndex = -1
+                            }
+                        })
+                        .onEnded({ _ in
+                            self.currentTouchedIndex = -1
+                        }))
+        }
+    }
+}
+
+#if DEBUG
+struct PieChartRow_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            PieChartRow(data: [PieChartData(value: 8), PieChartData(value: 23), PieChartData(value: 54), PieChartData(value: 32), PieChartData(value: 12), PieChartData(value: 37), PieChartData(value: 7), PieChartData(value: 23), PieChartData(value: 43)], backgroundColor: Color(red: 252.0/255.0, green: 236.0/255.0, blue: 234.0/255.0), accentColor: Color(red: 225.0/255.0, green: 97.0/255.0, blue: 76.0/255.0), showValue: Binding.constant(false), currentValue: Binding.constant(PieChartData(value: 0)))
+                .frame(width: 100, height: 100)
+            PieChartRow(data: [PieChartData(value: 0)], backgroundColor: Color(red: 252.0/255.0, green: 236.0/255.0, blue: 234.0/255.0), accentColor: Color(red: 225.0/255.0, green: 97.0/255.0, blue: 76.0/255.0), showValue: Binding.constant(false), currentValue: Binding.constant(PieChartData(value: 0)))
+                .frame(width: 100, height: 100)
+        }
+    }
+}
+#endif
---
+++ Sources/SwiftUICharts/PieChart/PieChartView.swift
@@ -0,0 +1,95 @@
+//
+//  PieChartView.swift
+//  ChartView
+//
+//  Created by András Samu on 2019. 06. 12..
+//  Copyright © 2019. András Samu. All rights reserved.
+//
+
+import SwiftUI
+
+public struct PieChartView: View {
+    public var data: [PieChartData]
+    public var title: String
+    public var legend: String?
+    public var style: ChartStyle
+    public var formSize: CGSize
+    public var dropShadow: Bool
+    public var valueSpecifier: String
+    public var prefix: String
+    public var postfix: String
+
+    @State private var showValue = false
+    @State private var currentValue: PieChartData = PieChartData(value: 0) {
+        didSet {
+            if oldValue.label != self.currentValue.label && self.showValue {
+                HapticFeedback.playSelection()
+            }
+        }
+    }
+
+    public init(data: [PieChartData], title: String, legend: String? = nil, style: ChartStyle = Styles.pieChartStyleOne, form: CGSize? = ChartForm.medium, dropShadow: Bool? = true, valueSpecifier: String? = "%.1f", prefix: String? = "", postfix: String? = "") {
+        self.data = data
+        self.title = title
+        self.legend = legend
+        self.style = style
+        self.formSize = form!
+        if self.formSize == ChartForm.large {
+            self.formSize = ChartForm.extraLarge
+        }
+        self.dropShadow = dropShadow!
+        self.valueSpecifier = valueSpecifier!
+        self.prefix = prefix!
+        self.postfix = postfix!
+    }
+
+    public var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(self.style.backgroundColor)
+                .cornerRadius(20)
+                .shadow(color: self.style.dropShadowColor, radius: self.dropShadow ? 12 : 0)
+            VStack(alignment: .leading) {
+                HStack {
+                    if !showValue {
+                        Text(self.title)
+                            .font(.system(size: 13, weight: .bold, design: .default))
+                            .foregroundColor(self.style.textColor)
+                    } else {
+                            Text("\(self.currentValue.label) \(self.prefix)- \(Int(self.currentValue.value))\(self.postfix)")
+                            .font(.system(size: 13, weight: .bold, design: .default))
+                                .foregroundColor(self.style.textColor)
+                    }
+                    Spacer()
+                    Image(systemName: "chart.pie.fill")
+                        .imageScale(.large)
+                        .foregroundColor(self.style.legendTextColor)
+                }.padding()
+                Spacer()
+//                if (currentValue.label.count <= 14) {
+//                    Text("              ")
+//                        .font(.headline)
+//                        .foregroundColor(self.style.textColor)
+//                }
+                PieChartRow(data: data, backgroundColor: self.style.backgroundColor, accentColor: self.style.accentColor, showValue: $showValue, currentValue: $currentValue)
+                    .foregroundColor(self.style.accentColor).padding(self.legend != nil ? 0 : 12).offset(y: self.legend != nil ? 0 : -10)
+                    .padding(0)
+                if self.legend != nil {
+                    Text(self.legend!)
+                        .font(.headline)
+                        .foregroundColor(self.style.legendTextColor)
+                        .padding()
+                }
+
+            }
+        }.frame(width: self.formSize.width, height: self.formSize.height)
+    }
+}
+
+#if DEBUG
+struct PieChartView_Previews: PreviewProvider {
+    static var previews: some View {
+        PieChartView(data: [PieChartData(label: "Q1", value: 56), PieChartData(label: "Q2", value: 78), PieChartData(label: "Q3", value: 53), PieChartData(label: "Q4", value: 65), PieChartData(label: "Q5", value: 54)], title: "Title", legend: "Legend")
+    }
+}
+#endif
